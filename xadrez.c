d5e5647c3e571adc2f0c818acb4f4d8cb41b458d
imavitor <vitor.imamura@protonmail.com>
2025-12-09 21:14:23 +0000

    Implementei o movimento do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
     int main (){
+        int c1=1; //variável para o movimento do cavalo
+
         //movimento da Torre em 5 casas para a direita
         printf("Torre\n");
         for (int t = 1; t <=5; t++){
@@ -12,10 +14,20 @@
             printf("Direita\n");
         }
         //movimento da Rainha em 8 casas para a esquerda
-        printf("Torre\n");
+        printf("Rainha\n");
         for (int r = 1; r <=8; r++){
             printf("Esquerda\n");
-    
         }
+        printf("\n"); //linha em branco para o movimento do cavalo
+        printf("Cavalo\n");
+        //implementando o código do movimento do cavalo
+        while(c1--){ //decremento do parâmetro c1 para o while imprimir esquerda uma vez
+            for (int c2 = 0; c2 < 2; c2++){
+                printf("Baixo\n");//imprime baixo duas vezes
+            }
+            printf("Esquerda");//imprime esquerda uma vez
+            
+        }
+
         return 0;
     }
