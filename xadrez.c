2e8a30cd8e822527499344cc18d38cab7d7273df
imavitor <vitor.imamura@protonmail.com>
2025-12-09 23:12:07 +0000

    Criei os loops aninhados e a recusividade

--- xadrez.c
+++ xadrez.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
-    int main (){
-        int c1=1; //variável para o movimento do cavalo
-
-        //movimento da Torre em 5 casas para a direita
-        printf("Torre\n");
-        for (int t = 1; t <=5; t++){
-            printf("Direita\n");
-        }
-        //movimento do Bispo em 5 casas para a direita
-        printf("Bispo\n");
-        for (int b = 1; b <=5; b++){
-            printf("Cima\n");
-            printf("Direita\n");
-        }
-        //movimento da Rainha em 8 casas para a esquerda
-        printf("Rainha\n");
-        for (int r = 1; r <=8; r++){
-            printf("Esquerda\n");
-        }
-        printf("\n"); //linha em branco para o movimento do cavalo
-        printf("Cavalo\n");
-        //implementando o código do movimento do cavalo
-        while(c1--){ //decremento do parâmetro c1 para o while imprimir esquerda uma vez
-            for (int c2 = 0; c2 < 2; c2++){
-                printf("Baixo\n");//imprime baixo duas vezes
+        //recursividade para o movimento da Torre em 5 casas para a direita
+        void moverTorre(int t) {
+            for (int t = 1; t <=5; t++){
+                printf("Direita\n");
             }
-            printf("Esquerda");//imprime esquerda uma vez
+        }
+        //recursividade para o movimento do Bispo em 5 casas para cima e 5 para a direita intercalando
+        void moverBispo(int b) {
+                for (int b = 1; b <=5; b++){// loop do movimento do bispo na vertical
+                    printf("Cima\n");
+                    for (int b2 = 1; b2 <=5; b2++){ // loop do movimento do bispo na horizontal, quando b é igual a b2 o código entende que está na hora de mostrar o movimento da horizontal
+                        if (b2==b){
+                            printf("Direita\n");
+                    }
+                    }
+            }
+        }
+        //recursividade para o movimento da rainha em 8 casas para a esquerda
+        void moverRainha(int r) {
+            for (int r = 1; r <=8; r++){
+                printf("Esquerda\n");
+                    }
+            }
+        //recursividade para o movimento do Cavalo duas casas pra cima e uma para a direita sem intercalar
+        void moverCavalo(int c) {
+            int i = 0;
+            for (int c = 1; c <=2; c++){
+                i++; //contador para o movimento da direita
+                printf("Cima\n");
+            }
+            if (i=2){
+                printf("Direita\n");
+            }
             
         }
-
-        return 0;
+        int main (){ //exibindo o movimento das peças e chamando a função recursiva
+            printf("Movimento da Torre: \n");
+            moverTorre(1);
+            printf("Movimento do Bispo: \n");
+            moverBispo(1);
+            printf("Movimento do Rainha: \n");
+            moverRainha(1);
+            printf("\n");//mantendo a linha em branco para o cavalo
+            printf("Movimento do Cavalo: \n");
+            moverCavalo(1);
+            return 0;
     }
